8985c8ec577f71a259664eb017add8e74b268b77
Meister1593 <leruop@gmail.com>
2025-08-29 10:14:13 +0200

    prepare test for files

--- src/tests/cmd/files.rs
+++ src/tests/cmd/files.rs
@@ -216,3 +216,52 @@ fn delete_file_tag() {
         .assert();
     assert.success().stdout("\n".to_string());
 }
+#[test]
+fn files_clean_after_unreference() {
+    let mut cmd = cargo_bin_cmd();
+
+    let temp_dir = TempDir::new().unwrap();
+    let (db_path, tag_file, _, test_tags, _) = two_files_multiple_tags_prepare(&temp_dir);
+
+    let assert = cmd
+        .arg("--database-path")
+        .arg(&db_path)
+        .arg("tag")
+        .arg(&tag_file)
+        .arg("--tags")
+        .arg("test,test2,test3")
+        .assert();
+    assert.success();
+
+    let mut cmd = cargo_bin_cmd();
+    let assert = cmd
+        .arg("--database-path")
+        .arg(&db_path)
+        .arg("tags")
+        .arg(&tag_file)
+        .assert();
+    assert
+        .success()
+        .stdout(format!("{}\n", test_tags.join(",")));
+
+    let mut cmd = cargo_bin_cmd();
+    let assert = cmd
+        .arg("--database-path")
+        .arg(&db_path)
+        .arg("tag")
+        .arg(&tag_file)
+        .arg("--tags")
+        .arg("test,test2,test3")
+        .arg("--delete")
+        .assert();
+    assert.success();
+
+    let mut cmd = cargo_bin_cmd();
+    let assert = cmd
+        .arg("--database-path")
+        .arg(&db_path)
+        .arg("tags")
+        .arg(&tag_file)
+        .assert();
+    assert.success().stdout("\n".to_string());
+}
--- src/tests/files.rs
+++ src/tests/files.rs
@@ -95,3 +95,22 @@ fn delete_file_tag() {
     let tags = commands::tags::get_file_tags(&db, &tag_file).unwrap();
     assert_eq!("", tags);
 }
+
+#[test]
+fn files_clean_after_unreference() {
+    // Test data
+    let temp_dir = TempDir::new().unwrap();
+    let (db_path, tag_file, _, test_tags, _) = super::two_files_multiple_tags_prepare(&temp_dir);
+
+    let mut db = Database::new(&DatabaseMode::ReadWrite, &db_path);
+    commands::tag::tag_file(&mut db, &tag_file, &test_tags, false).unwrap();
+    let db = Database::new(&DatabaseMode::Read, &db_path);
+    let tags = commands::tags::get_file_tags(&db, &tag_file).unwrap();
+    assert_eq!(test_tags.join(","), tags);
+
+    let mut db = Database::new(&DatabaseMode::ReadWrite, &db_path);
+    commands::tag::tag_file(&mut db, &tag_file, &test_tags, true).unwrap();
+    let db = Database::new(&DatabaseMode::Read, &db_path);
+    let tags = commands::tags::get_file_tags(&db, &tag_file).unwrap();
+    assert_eq!("", tags);
+}
